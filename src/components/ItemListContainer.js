d701944be410c65004f74ba385e5f2cadead7d37
Ezequiel Hermoso <hermosoezequiel352@gmail.com>
2022-11-01 18:01:54 -0300

    primera entrega

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+
+
+function ItemListContainer({greetings}) {
+    return (
+      <body className="body">
+        <div className="contenedorBody">
+            <p className="parrafoBody">{greetings}</p>
+        </div>
+      </body>
+    )
+  }
+  export default ItemListContainer;
